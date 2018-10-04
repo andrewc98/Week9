f2fc09123e0575a9143ab23c230bdae9993d4c45
Andrew Campbell <andrew.campbell7@griffithuni.edu.au>
2018-10-05 07:23:35 +1000

    Removed old routes, installed mocha on server.

--- server/app.js
+++ server/app.js
@@ -97,37 +97,3 @@ app.post('/api/products', (req, res) => {
         db.close();
     });
 });
-
-app.get('/home',function(request,response){
-    response.sendFile(__dirname + "/www/index.html");
-    MongoClient.connect(url, {poolSize:10}, function(err, db) {
-        if (err) { return console.log(err) }
-        const dbName = 'mydb';
-        var products = db.db(dbName);
-    
-        // Drop & Create products
-        const create = require('./create.js');
-        create.createDatabase(products);
-        // Drop & Create products
-    
-        // Insert products
-        const insert = require('./add.js');
-        insert.addProduct(products);
-        // Insert products
-    
-        // Delete a product
-        const remove = require('./remove.js');
-        remove.deleteProduct(products);
-        // Delete a product
-    
-        // Update a product
-        const update = require('./update.js');
-        update.updateProduct(products);
-        // Update a product
-    
-        // Find products
-        const find = require('./read.js');
-        find.readProducts(products, db);
-        // Find products
-    });
-});
