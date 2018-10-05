3bb5cfe2057cfe527bc85ee8e50dfd4b393d3958
Andrew Campbell <andrew.campbell7@griffithuni.edu.au>
2018-10-05 10:07:33 +1000

    Two tests performing well.

--- server/test/test.js
+++ server/test/test.js
@@ -1,17 +1,59 @@
-var server = require('../app');
-var chai = require('chai');
-var supertest = require('supertest');
+var assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/mydb';
+const mongo_parser = require('mongodb');
 
-var expect = chai.expect;
-describe('API Tests', function() {
-    describe('# Get all tasks', function() { 
-        it('should get all tasks', function(done) { 
-            request(server) .get('/tasks') .end(function(err, res) { 
-                expect(res.statusCode).to.equal(200); 
-                expect(res.body).to.be.an('array'); 
-                expect(res.body).to.be.empty; 
-                done(); 
+describe('Tests', function() {
+    describe('All Products 1', function() {
+        this.timeout(10000);
+        var read_results;
+        before((done) => {
+            MongoClient.connect(url, {poolSize:10}, function(err, db) {
+                if (err) { return console.log(err) }
+                const dbName = 'mydb';
+                var products = db.db(dbName);
+                products.collection("products").find({}).toArray(function(err, result) {
+                    if (err) { return console.log(err) }
+                    read_results = result;
+                    done();
+                });
+                db.close();
             });
-        }); 
-    }); 
+        });
+
+        it('Find Products', () => {
+            assert.equal(typeof read_results, 'object');
+        });
+
+        after((done) => {
+            done();
+        });
+    });
+    describe('All Products', function() {
+        this.timeout(10000);
+        var read_results;
+
+        before((done) => {
+
+            MongoClient.connect(url, {poolSize:10}, function(err, db) {
+                if (err) { return console.log(err) }
+                const dbName = 'mydb';
+                var products = db.db(dbName);
+                products.collection("products").findOne({name: 'asd'}, function(err, result) {
+                    if (err) { return console.log(err) }
+                    read_results = result;
+                    done();
+                });
+                db.close();
+            });
+        });
+
+        it('Find Products', () => {
+            assert.equal(read_results.name, 'asd');
+        });
+
+        after((done) => {
+            done();
+        });
+    });
 });
