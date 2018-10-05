c056f7c4bfbc889f6f9640daf374214400b2e199
Andrew Campbell <andrew.campbell7@griffithuni.edu.au>
2018-10-05 11:12:21 +1000

    Update works alright.

--- server/test/test.js
+++ server/test/test.js
@@ -40,7 +40,7 @@ describe('Read', function() {
                 if (err) { return console.log(err) }
                 const dbName = 'mydb';
                 var products = db.db(dbName);
-                products.collection("products").findOne({name: 'asd'}, function(err, result) {
+                products.collection("products").findOne({}, function(err, result) {
                     if (err) { return console.log(err) }
                     read_results = result;
                     done();
@@ -50,7 +50,7 @@ describe('Read', function() {
         });
 
         it('Find Products', () => {
-            assert.equal(read_results.name, 'asd');
+            assert.equal(typeof read_results.name, 'string');
         });
 
         after((done) => {
@@ -113,6 +113,9 @@ describe('Read', function() {
     });
 });
 
+
+
+
 describe('Add', function() {
     describe('Add Products 1', function() {
         this.timeout(10000);
@@ -122,7 +125,7 @@ describe('Add', function() {
                 if (err) { return console.log(err) }
                 const dbName = 'mydb';
                 var products = db.db(dbName);
-                products.collection("products").insertOne({name: 'hi'}, function(err, res) {
+                products.collection("products").insertOne({_id: 1, name: 'hi', price: 1, type: "a", desc: "b"}, function(err, res) {
                     if (err) { return console.log(err) }
                     add_results = res.insertedCount;
                     done();
@@ -131,7 +134,7 @@ describe('Add', function() {
             });
         });
 
-        it('Find Products', () => {
+        it('Add Products', () => {
             assert.equal(add_results, 1);
         });
 
@@ -148,7 +151,7 @@ describe('Add', function() {
                 if (err) { return console.log(err) }
                 const dbName = 'mydb';
                 var products = db.db(dbName);
-                products.collection("products").insertOne({name: 'hi'}, function(err, res) {
+                products.collection("products").insertOne({_id: 3, name: 'hi'}, function(err, res) {
                     if (err) { return console.log(err) }
                     add_results = res;
                     done();
@@ -157,7 +160,7 @@ describe('Add', function() {
             });
         });
 
-        it('Find Products', () => {
+        it('Add Products', () => {
             assert.equal(add_results.ops[0].name, 'hi');
         });
 
@@ -183,7 +186,7 @@ describe('Add', function() {
             });
         });
 
-        it('Find Products', () => {
+        it('Add Products', () => {
             assert.equal(add_results, 2);
         });
 
@@ -209,8 +212,116 @@ describe('Add', function() {
             });
         });
 
-        it('Find Products', () => {
+        it('Add Products', () => {
             assert.equal(add_results.ops[0].name, 'Hello');
+        });
+
+        after((done) => {
+            done();
+        });
+    });
+});
+
+
+
+
+describe('Update', function() {
+    describe('Update Products 1', function() {
+        this.timeout(10000);
+        var add_results;
+        before((done) => {
+            MongoClient.connect(url, {poolSize:10}, function(err, db) {
+                if (err) { return console.log(err) }
+                const dbName = 'mydb';
+                var products = db.db(dbName);
+                products.collection("products").findOneAndUpdate({_id: 1}, { $set: {name: 'Hello World'}}, { new: true }, function(err, res) {
+                    if (err) { return console.log(err) }
+                    add_results = res;
+                    done();
+                });
+                db.close();
+            });
+        });
+
+        it('Update Products', () => {
+            assert.equal(add_results.value.name, 'Hello World');
+        });
+
+        after((done) => {
+            done();
+        });
+    });
+    describe('Update Products 2', function() {
+        this.timeout(10000);
+        var add_results;
+        before((done) => {
+            MongoClient.connect(url, {poolSize:10}, function(err, db) {
+                if (err) { return console.log(err) }
+                const dbName = 'mydb';
+                var products = db.db(dbName);
+                products.collection("products").findOneAndUpdate({_id: 3}, { $set: {name: 'Hello Worlds'}}, { new: true }, function(err, res) {
+                    if (err) { return console.log(err) }
+                    add_results = res;
+                    done();
+                });
+                db.close();
+            });
+        });
+
+        it('Update Products', () => {
+            assert.equal(add_results.value.desc, 'Hello Worlds');
+        });
+
+        after((done) => {
+            done();
+        });
+    });
+
+    describe('Update Products 3', function() {
+        this.timeout(10000);
+        var add_results;
+        before((done) => {
+            MongoClient.connect(url, {poolSize:10}, function(err, db) {
+                if (err) { return console.log(err) }
+                const dbName = 'mydb';
+                var products = db.db(dbName);
+                products.collection("products").findOneAndUpdate({_id: 2}, { $set: {name: 'Hello World'}}, { new: true }, function(err, res) {
+                    if (err) { return console.log(err) }
+                    add_results = res;
+                    done();
+                });
+                db.close();
+            });
+        });
+
+        it('Update Products', () => {
+            assert.equal(add_results.value.name, 'Hi');
+        });
+
+        after((done) => {
+            done();
+        });
+    });
+
+    describe('Update Products 4', function() {
+        this.timeout(10000);
+        var add_results;
+        before((done) => {
+            MongoClient.connect(url, {poolSize:10}, function(err, db) {
+                if (err) { return console.log(err) }
+                const dbName = 'mydb';
+                var products = db.db(dbName);
+                products.collection("products").findOneAndUpdate({_id: 2}, { $set: {name: 'This'}}, { new: true }, function(err, res) {
+                    if (err) { return console.log(err) }
+                    add_results = res;
+                    done();
+                });
+                db.close();
+            });
+        });
+
+        it('Update Products', () => {
+            assert.equal(add_results.value.desc, 'This');
         });
 
         after((done) => {
